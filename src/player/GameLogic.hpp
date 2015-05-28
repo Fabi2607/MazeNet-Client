82bdf5e36724e293726936809ec50b07974fbd19
Fabian Kantereit <fabian.kantereit@dark-dev.com>
2015-05-28 17:02:02 +0200

    Applying for Idiot of the Month...

--- src/player/GameLogic.hpp
+++ src/player/GameLogic.hpp
@@ -6,8 +6,15 @@
 #define MAZENET_CLIENT_GAMELOGIC_H
 
 
+#include "Board.hpp"
+#include "Player.hpp"
+
+#include <set>
+#include <messaging/MessageDispatcher.hpp>
+
 class GameLogic {
  public:
+  GameLogic() = default;
   int getPlayer_id() const {
     return player_id_;
   }
@@ -16,8 +23,16 @@ class GameLogic {
     player_id_ = player_id;
   }
 
+  Board board_ = Board();
+  Card shiftCard_ = Card();
+  int forbidden_row_ = -1;
+  int forbidden_col_ = -1;
+  Player players_[4];
+  int treasure_ = -1;
+  int player_count_ = -1;
+  std::set<int> found_treasures_;
  private:
-  int player_id_;
+  int player_id_ = -1;
 };
 
 
