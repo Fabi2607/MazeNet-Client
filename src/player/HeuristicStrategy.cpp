27967ffdba6385985927ebc7ce3a360685532758
Fabian Kantereit <fabian.kantereit@dark-dev.com>
2015-06-18 15:42:11 +0200

    fixed more errors and improved ai a bit

--- src/player/HeuristicStrategy.cpp
+++ src/player/HeuristicStrategy.cpp
@@ -10,6 +10,7 @@ HeuristicStrategy::HeuristicStrategy() : settings_() { }
 
 Move HeuristicStrategy::calculate_next_move() {
   using namespace mazenet::util::logging;
+  auto t1 = std::chrono::high_resolution_clock::now();
 
   std::future<int> result;
   std::future<int> fireResult;
@@ -23,7 +24,7 @@ Move HeuristicStrategy::calculate_next_move() {
 
   Log logger("heuristic");
   Move best_move;
-  int best_score = 0;
+  int best_score = INT32_MIN;
   logger.logSeverity(SeverityLevel::info) << "Evaluating: \n" << situation_.board_
   << "\nShift Card: " << situation_.shiftCard_
   << "\nPlayer Pos: " << "R: " << situation_.players_[situation_.player_id_ - 1].pos_.row
@@ -41,9 +42,10 @@ Move HeuristicStrategy::calculate_next_move() {
       }
     }
   }
+
 #pragma omp parallel
   {
-    int local_best_score = 0;
+    int local_best_score = INT32_MIN;
     Move local_best_move;
 
 #pragma omp for reduction(+:situations)
@@ -88,8 +90,10 @@ Move HeuristicStrategy::calculate_next_move() {
       }
     }
   }
+  auto t2 = std::chrono::high_resolution_clock::now();
 
-  logger.logSeverity(SeverityLevel::notification) << "Calculated situations: " << situations << logger.end();
+  logger.logSeverity(SeverityLevel::notification) << "Calculated situations: " << situations
+  << "\nTime: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << logger.end();
 
   logger.logSeverity(SeverityLevel::notification) << "Best Move: (" << best_score << ")"
   << "\nCard: " << best_move.shift_card << " R: " << best_move.shift_pos.row << " C: " << best_move.shift_pos.col
@@ -137,7 +141,7 @@ int HeuristicStrategy::evaluate_position_score(const GameSituation& situation, c
 
   auto& cur_card = situation.board_.cards_[position.row][position.col];
 
-//  Position target_pos;
+  bool treasureFound = false;
 
   if (position.row == -1 && position.col == -1) {
     score -= settings_.moveOutOfMapPenalty;
@@ -145,6 +149,7 @@ int HeuristicStrategy::evaluate_position_score(const GameSituation& situation, c
 
   if (cur_card.getTreasure() == situation.treasure_) {
     score += settings_.onTreasureScore;
+    treasureFound = true;
   } else {
     // distance score
     for (int row = 0; row < 7; ++row) {
@@ -152,7 +157,6 @@ int HeuristicStrategy::evaluate_position_score(const GameSituation& situation, c
         if (situation.board_.cards_[row][col].getTreasure() == situation.treasure_) {
           int distance = ::abs(position.row - row) + ::abs(position.col - col);
 
-//          target_pos = {row, col};
           score += settings_.distanceBaseValue / (distance * settings_.distanceFactor);
         };
       }
@@ -167,16 +171,24 @@ int HeuristicStrategy::evaluate_position_score(const GameSituation& situation, c
       auto positions = MoveCalculator::get_possible_positions(cur_sit);
 
       for (auto pos: positions) {
-        if (cur_sit.board_.cards_[pos.row][pos.col].getTreasure() == situation.treasure_) {
-          score += settings_.transitionMoveValue;
+        if (treasureFound) {
+          if (cur_sit.board_.cards_[pos.row][pos.col].getTreasure() > 3 &&
+              cur_sit.found_treasures_.find(cur_sit.board_.cards_[pos.row][pos.col].getTreasure()) ==
+              cur_sit.found_treasures_.end()) {
+            score += settings_.transitionPossibleTreasureValue;
+          }
+        } else {
+          if (cur_sit.board_.cards_[pos.row][pos.col].getTreasure() == situation.treasure_) {
+            score += settings_.transitionMoveValue;
 
-          auto next_moves = MoveCalculator::get_possible_moves(situation);
-          GameSituation copy_sit = cur_sit;
-          copy_sit.perform_shift(move);
-          auto next_positions = MoveCalculator::get_possible_positions(copy_sit);
-          for(auto next_pos : positions) {
-            if (copy_sit.board_.cards_[next_pos.row][next_pos.col].getTreasure() == situation.treasure_) {
-              score += settings_.transitionMoveValue / 2;
+            auto next_moves = MoveCalculator::get_possible_moves(situation);
+            GameSituation copy_sit = cur_sit;
+            copy_sit.perform_shift(move);
+            auto next_positions = MoveCalculator::get_possible_positions(copy_sit);
+            for (auto next_pos : positions) {
+              if (copy_sit.board_.cards_[next_pos.row][next_pos.col].getTreasure() == situation.treasure_) {
+                score += settings_.transitionMoveValue / 2;
+              }
             }
           }
         }
@@ -207,11 +219,13 @@ int HeuristicStrategy::evaluate_enemy_score(const GameSituation& situation) {
     new_sit.player_id_ = best_id;
     auto positions = MoveCalculator::get_possible_positions(new_sit);
     for (auto pos : positions) {
-      if (new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) !=
+      if (new_sit.board_.cards_[pos.row][pos.col].getTreasure() > 3 &&
+          new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) ==
           new_sit.found_treasures_.end()) {
         best_score += settings_.bestReachableTreasureValue;
       }
     }
+
 
     auto moves = MoveCalculator::get_possible_moves(new_sit);
     for (auto move : moves) {
@@ -222,20 +236,27 @@ int HeuristicStrategy::evaluate_enemy_score(const GameSituation& situation) {
       auto trans_positions = MoveCalculator::get_possible_positions(transitionSit);
 
       for (auto pos : trans_positions) {
-        if (new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) !=
+        if (new_sit.board_.cards_[pos.row][pos.col].getTreasure() > 3 &&
+            new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) ==
             new_sit.found_treasures_.end()) {
           best_score += settings_.bestTransitionMoveTreasureValue;
+
+          Position home = new_sit.players_[best_id - 1].home_;
+          if ((best_remaining <= settings_.bestReachableHomeCutoff) &&
+              (home.row == pos.row && home.col == pos.col)) {
+            best_score += settings_.bestReachableHomeValue;
+          }
         }
       }
     }
 
-    score += best_score/best_remaining; // adjust for remaining treasure scaling
+    score += best_score / best_remaining; // adjust for remaining treasure scaling
   }
 
   // next player score
   int next_id = (situation.player_id_ % situation.player_count_) + 1;
 
-  if(next_id != situation.player_id_) {
+  if (next_id != situation.player_id_) {
     int next_score = 0;
 
     GameSituation new_sit = situation;
@@ -243,7 +264,8 @@ int HeuristicStrategy::evaluate_enemy_score(const GameSituation& situation) {
     new_sit.player_id_ = next_id;
     auto positions = MoveCalculator::get_possible_positions(new_sit);
     for (auto pos : positions) {
-      if (new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) !=
+      if (new_sit.board_.cards_[pos.row][pos.col].getTreasure() > 3 &&
+          new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) ==
           new_sit.found_treasures_.end()) {
         next_score += settings_.nextReachableTreasureValue;
       }
@@ -258,14 +280,21 @@ int HeuristicStrategy::evaluate_enemy_score(const GameSituation& situation) {
       auto trans_positions = MoveCalculator::get_possible_positions(transitionSit);
 
       for (auto pos : trans_positions) {
-        if (new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) !=
+        if (new_sit.board_.cards_[pos.row][pos.col].getTreasure() > 3 &&
+            new_sit.found_treasures_.find(new_sit.board_.cards_[pos.row][pos.col].getTreasure()) ==
             new_sit.found_treasures_.end()) {
           next_score += settings_.nextTransitionMoveTreasureValue;
+        }
+
+        Position home = new_sit.players_[best_id - 1].home_;
+        if ((situation.players_[next_id - 1].remainingTreasures_ <= settings_.nextReachableHomeCutoff) &&
+            (home.row == pos.row && home.col == pos.col)) {
+          next_score += settings_.nextReachableHomeValue;
         }
       }
     }
 
-    score += next_score/situation.players_[next_id-1].remainingTreasures_; // adjust for remaining treasure scaling
+    score += next_score / situation.players_[next_id - 1].remainingTreasures_; // adjust for remaining treasure scaling
   }
 
   return score;
